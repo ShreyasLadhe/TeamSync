4dd34064ba622b778581dbe6e8be9a755b2dccb7
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2025-05-11 11:13:56 +0530

    Update index.ts for cors

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,8 @@ app.use(
   cors({
     origin: config.FRONTEND_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
 
