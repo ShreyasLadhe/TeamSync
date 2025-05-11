72b611a900195bb133798743c7669ae57edf15f1
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2025-05-11 12:24:54 +0530

    Update declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,13 @@
-declare module '*.css';
-declare module '*.json';
-declare module 'express';
-declare module 'passport';
+import { Request } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any; // Replace `any` with your user type if available
+      workspaceName?: string;
+      params: Record<string, string>; // or { [key: string]: string }
+    }
+  }
+}
+
+export {}; // Required to treat this as a module
